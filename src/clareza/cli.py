e5045e76d374c0170e0fe20623617abebea23b5d
Akasha-0 <akasha.arth@gmail.com>
2026-05-09 23:07:46 -0300

    auto-claude: subtask-2-2 - Implement version command

--- src/clareza/cli.py
+++ src/clareza/cli.py
@@ -4,7 +4,7 @@ import click
 
 
 @click.group()
-@click.version_option(version="0.1.0")
+@click.version_option(version="0.1.0", prog_name="clareza", message="%(prog)s %(version)s\n")
 def cli() -> None:
     """Clareza - Ferramenta CLI para Baralho Cigano."""
     pass
